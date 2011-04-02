a27ea5fd8a7596fa8087d42dd873620eb63e66f0
Rob Zienert <rob@robzienert.com>
2011-04-02 11:54:14 -0400

    di extension; orm metadata

---
+++ DependencyInjection/RizzaVotingExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Rizza\VotingBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Config\FileLocator;
+
+class RizzaVotingExtension extends Extension
+{
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+
+        $loader->load('orm.xml');
+    }
+
+    public function getAlias()
+    {
+        return 'rizza_voting';
+    }
+}
