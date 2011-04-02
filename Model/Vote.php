2941485ca4757e841adaf5e1880d904c5c2a1da9
Rob Zienert <rob@robzienert.com>
2011-04-02 10:49:50 -0400

    vote model

---
+++ Model/Vote.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Rizza\VotingBundle\Model;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+abstract class Vote implements VoteInterface
+{
+    const VOTE_UP   = 1;
+    const VOTE_DOWN = -1;
+    const VOTE_NULL = 0;
+
+    protected $id;
+    
+    protected $user;
+
+    protected $object;
+
+    protected $value;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setUser(UserInterface $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function setObject(Votable $object)
+    {
+        $this->object = $object;
+    }
+
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    public function setValue($value)
+    {
+        $this->value = (int) $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function isUpvote()
+    {
+        return self::VOTE_UP == $this->value;
+    }
+
+    public function isDownvote()
+    {
+        return self::VOTE_DOWN == $this->value;
+    }
+}
