b4e0e53f2ed4143648e04b8db79175a7630828b6
Rob Zienert <rob@robzienert.com>
2011-04-02 10:42:18 -0400

    updating the vote model interface

--- Model/VoteInterface.php
+++ Model/VoteInterface.php
@@ -4,11 +4,38 @@ namespace Rizza\VotingBundle\Model;
 
 interface VoteInterface
 {
+    /**
+     * Returns true if the vote is an upvote.
+     *
+     * @return bool
+     */
     public function isUpvote();
 
+    /**
+     * Returns true if the vote is a downvote.
+     *
+     * @return bool
+     */
     public function isDownvote();
 
+    /**
+     * Returns the actual vote value.
+     *
+     * @return int
+     */
+    public function getValue();
+
+    /**
+     * Returns the owning user.
+     *
+     * @return Symfony\Component\Security\Core\User\UserInterface
+     */
     public function getUser();
 
+    /**
+     * Returns the subject Votable object.
+     *
+     * @return Votable
+     */
     public function getObject();
 }
